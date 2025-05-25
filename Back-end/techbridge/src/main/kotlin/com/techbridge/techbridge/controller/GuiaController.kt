64b425fed67aaeb00d637263049770230f219177
PedroTrovoSouza <pedro.tsouza@solutis.com.br>
2025-05-25 19:32:10 -0300

    fiFuncionalidade do guia cadastrar um evento

--- Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/controller/GuiaController.kt
+++ Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/controller/GuiaController.kt
@@ -1,10 +1,28 @@
-/*package com.techbridge.techbridge.controller
+package com.techbridge.techbridge.controller
 
-import org.springframework.web.bind.annotation.*
+import com.techbridge.techbridge.dto.EventoRequestDTO
+import com.techbridge.techbridge.service.GuiaService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/guia")
-class GuiaController() {
+class GuiaController {
 
+    @Autowired
+    lateinit var eventoService: GuiaService
 
-}*/
+    @PostMapping("/cadastrar-evento")
+    fun postEvento(@RequestBody novoEvento: EventoRequestDTO): ResponseEntity<Any>{
+        return try {
+            val eventoSalvo = eventoService.postEvento(novoEvento);
+            ResponseEntity.ok(novoEvento)
+        }catch (e: RuntimeException){
+            ResponseEntity.status(400).body(e.message)
+        }
+    }
+}
