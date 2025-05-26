9f5b5d704cb7285cb225c7ac3a29ba8906a152bf
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2025-05-25 22:14:49 -0300

    Update EventoRepository.kt

--- Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/repository/EventoRepository.kt
+++ Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/repository/EventoRepository.kt
@@ -1,7 +1,27 @@
 package com.techbridge.techbridge.repository
 
+import com.techbridge.techbridge.dto.EventoGuiaEnderecoDTO
+
 import com.techbridge.techbridge.entity.Evento
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface EventoRepository: JpaRepository<Evento, Long> {
+    @Query(
+        value = """
+        SELECT 
+            e.id_evento,
+            e.nome AS nome_evento,
+            e.descricao,
+            e.nivel_dificuldade,
+            e.distancia_km,
+            u.nome AS nome_responsavel,
+            end.rua
+        FROM evento e
+        JOIN usuario u ON e.responsavel_id = u.id_usuario
+        JOIN endereco end ON e.endereco_id = end.id_endereco
+    """,
+        nativeQuery = true
+    )
+    fun buscarTodosEventosComUsuarioEndereco(): List<Map<String, Any>>
 }
