b928922e7e3001094f9438cec701e60f11958240
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2025-05-25 22:16:37 -0300

    Update GuiaController.kt

--- Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/controller/GuiaController.kt
+++ Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/controller/GuiaController.kt
@@ -1,9 +1,12 @@
 package com.techbridge.techbridge.controller
 
+import com.techbridge.techbridge.dto.EventoGuiaEnderecoDTO
 import com.techbridge.techbridge.dto.EventoRequestDTO
+import com.techbridge.techbridge.dto.EventoResponseDTO
 import com.techbridge.techbridge.service.GuiaService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -23,6 +26,16 @@ class GuiaController {
             ResponseEntity.ok(novoEvento)
         }catch (e: RuntimeException){
             ResponseEntity.status(400).body(e.message)
+        }
+    }
+
+    @GetMapping("/buscar-eventos")
+    fun getAllEventos(): ResponseEntity<Any>{
+        return try {
+            val eventosEncontrados: List<Map<String, Any>> = eventoService.getEventos();
+            return ResponseEntity.status(200).body(eventosEncontrados)
+        }catch (e: RuntimeException){
+           return ResponseEntity.status(404).body(e.message)
         }
     }
 }
