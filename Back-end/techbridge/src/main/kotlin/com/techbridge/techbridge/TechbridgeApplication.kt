2c76539417f0333e3ecfda362b25fa7d288d201f
RebecaCagni <rebeca.cagni@sptech.school>
2025-08-26 23:23:52 -0300

    Adicionando configurações para CORS

--- Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/TechbridgeApplication.kt
+++ Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/TechbridgeApplication.kt
@@ -2,6 +2,19 @@ package com.techbridge.techbridge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@SpringBootApplication
+class Application : WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:5173")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+	}
+}
+
 
 @SpringBootApplication
 class TechbridgeApplication
