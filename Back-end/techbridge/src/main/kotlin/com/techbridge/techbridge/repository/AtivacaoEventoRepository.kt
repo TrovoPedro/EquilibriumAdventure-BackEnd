22c94819ff65c577e931b597e2892c2feca54e37
edro01pinho <pedro.antunes@sptech.school>
2025-10-09 22:54:37 -0300

    alterando fun do ativacaoeventoRepository

--- Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/repository/AtivacaoEventoRepository.kt
+++ Back-end/techbridge/src/main/kotlin/com/techbridge/techbridge/repository/AtivacaoEventoRepository.kt
@@ -3,7 +3,12 @@ package com.techbridge.techbridge.repository
 import com.techbridge.techbridge.entity.AtivacaoEvento
 import com.techbridge.techbridge.enums.EstadoEvento
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface AtivacaoEventoRepository : JpaRepository<AtivacaoEvento, Long>{
-    fun findByEventoIdAndEstadoIn(eventoId: Long, estados: List<EstadoEvento>): AtivacaoEvento?
-}
+    @Query("SELECT a FROM AtivacaoEvento a WHERE a.evento.id_evento = :eventoId AND a.estado IN :estados")
+    fun findByEventoIdAndEstadoIn(
+        @Param("eventoId") eventoId: Long,
+        @Param("estados") estados: List<EstadoEvento>
+    ): AtivacaoEvento?}
